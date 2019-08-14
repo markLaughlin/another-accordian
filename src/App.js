bfbe5357ea8158b5c753bb9f6be1a64ce23cacdb
Mark Laughlin <mlaughlin70@gmail.com>
2019-08-14 10:53:54 -0500

    added three methods

--- src/App.js
+++ src/App.js
@@ -7,29 +7,52 @@ class App extends React.Component {
     super(props)
 
     this.state = {
-      message: "Inspirational message to come..."
+      currentNumber: 0,
+      quotes: ["Inspirational message to come...",
+      "The harder you worker, the luckier you get!",
+              "The darkest hour is before the dawn.",
+              "You can do it if you try!"]
     }
+    this.handleClickOne = this.handleClickOne.bind(this);
+    this.handleClickTwo = this.handleClickTwo.bind(this);
+    this.handleClickThree = this.handleClickThree.bind(this);
+  }
+
+  handleClickOne(){
+    console.log("handleClickOne method ran")
+    this.setState({currentNumber: 1})
+  }
+
+  handleClickTwo(){
+    console.log("handleClickTwo method ran")
+    this.setState({currentNumber: 2})
+  }
+
+  handleClickThree(){
+    console.log("handleClickThree method ran")
+    this.setState({currentNumber: 3})
   }
 
   render(){
+    console.log("render method of App component ran")
     return (
       <div className="App">
        <h1>Hello from the App component of the Accordian App</h1>
-       <button>Click for Inspirational Message One</button>
+       <button onClick={this.handleClickOne}>Click for Inspirational Message One</button>
        <br/>
        <br/>
 
 
-       <button>Click for Inspirational Message Two</button>
+       <button onClick={this.handleClickTwo}>Click for Inspirational Message Two</button>
        <br/>
        <br/>
 
 
-       <button>Click for Inspirational Message Three</button>
+       <button onClick={this.handleClickThree}> Click for Inspirational Message Three</button>
        <br/>
        <br/>
 
-       {this.state.message}
+       {this.state.quotes[this.state.currentNumber]}
 
       </div>
     );
