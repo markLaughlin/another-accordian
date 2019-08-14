6ef140697b2407e2d903c5e04329643271f5e2ea
Mark Laughlin <mlaughlin70@gmail.com>
2019-08-14 12:20:18 -0500

    More changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ class App extends React.Component {
     super(props)
 
     this.state = {
-      currentNumber: 0,
+      count: 0,
+      seenOne: false,
+      seenTwo: false,
+      seenThree: false,
       quotes: ["Inspirational message to come...",
       "The harder you worker, the luckier you get!",
               "The darkest hour is before the dawn.",
@@ -20,24 +23,41 @@ class App extends React.Component {
 
   handleClickOne(){
     console.log("handleClickOne method ran")
-    this.setState({currentNumber: 1})
+    this.setState({count: this.state.count + 1})
+    this.setState({seenOne: true})
+    console.log("this.state.seenOne: ")
+    console.log(this.state.seenOne)
   }
 
   handleClickTwo(){
     console.log("handleClickTwo method ran")
-    this.setState({currentNumber: 2})
+    this.setState({count: this.state.count + 1})
+    this.setState({seenTwo: true})
+    console.log("this.state.seenTwo: ")
+    console.log(this.state.seenTwo)
   }
 
   handleClickThree(){
     console.log("handleClickThree method ran")
-    this.setState({currentNumber: 3})
+    this.setState({count: this.state.count + 1})
+    this.setState({seenThree: true})
+    console.log("this.state.seenThree: ")
+    console.log(this.state.seenThree)
   }
 
   render(){
     console.log("render method of Mark's App component ran")
+    console.log("this.state.seenOne: ")
+    console.log(this.state.seenOne)
+    console.log("this.state.seenTwo: ")
+    console.log(this.state.seenTwo)
+    console.log("this.state.seenThree: ")
+    console.log(this.state.seenThree)
 
-    let condit = this.state.currentNumber > 2 ? <p>Hit refresh to clear messages!</p> : ""
-
+    let seenAll = (this.state.seenOne === true && this.state.seenTwo ===true && this.state.seenThree === true) ? 
+    <p>You've seen all three messages!</p> : 
+    ""
+ 
     return (
       <div className="App">
        <h1>Hello from the App component of Mark's Most Excellent Accordian App</h1>
@@ -56,8 +76,7 @@ class App extends React.Component {
        <br/>
 
        {this.state.quotes[this.state.currentNumber]}
-       <br/>
-       {condit}
+       {seenAll}
       </div>
     );
   }
