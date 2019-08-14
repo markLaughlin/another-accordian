bc6d3399bc6084b57ae6a0acca6535da125b303a
Mark Laughlin <mlaughlin70@gmail.com>
2019-08-14 10:31:50 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      message: "Inspirational message to come..."
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+       <h1>Hello from the App component of the Accordian App</h1>
+       <button>Click for Inspirational Message One</button>
+       <br/>
+       <br/>
+
+
+       <button>Click for Inspirational Message Two</button>
+       <br/>
+       <br/>
+
+
+       <button>Click for Inspirational Message Three</button>
+       <br/>
+       <br/>
+
+       {this.state.message}
+
+      </div>
+    );
+  }
 }
 
 export default App;
